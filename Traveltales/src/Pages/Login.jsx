f671bb0b6a459a5093db6669e2e2b7962cec94f3
Sanjup22 <sanjanapandey1979@gmail.com>
2025-10-17 19:30:11 +0530

    "fixes"

--- Traveltales/src/Pages/Login.jsx
+++ Traveltales/src/Pages/Login.jsx
@@ -19,7 +19,14 @@ export default function Login() {
     const payload = { email, password, mode };
     if (mode === "signup") payload.confirmPassword = confirm;
 
-    const res = await axios.post("https://traveltales-l1a7.onrender.com/api/login", payload);
+const res = await axios.post(
+  "https://traveltales-l1a7.onrender.com/api/login",
+  payload,
+  {
+    headers: { "Content-Type": "application/json" },
+  }
+);
+
 
     if (res.data.success) {
       localStorage.setItem("token", res.data.token);
