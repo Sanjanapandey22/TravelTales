8a75df12f768353e696bf9219faead1c9393c7ee
Sanjup22 <sanjanapandey1979@gmail.com>
2025-10-22 16:13:55 +0530

    "small fix"

--- traveltales-backend/models/Post.js
+++ traveltales-backend/models/Post.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    location: { type: String },
-    createdAt: { type: Date, default: Date.now }
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  category: { type: String, default: "general" },
+  image: { type: String, default: "/default.jpg" },
+  author: { type: String, default: "User" },
+  createdAt: { type: Date, default: Date.now }
 });
+
 
 const Post = mongoose.model("Post", postSchema);
 
